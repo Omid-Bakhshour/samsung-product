e32b9504663e95e8f3938d1b4fbfe3ff278cf4ff
Omid <fishvardroid@gmail.com>
2024-07-26 17:42:13 +0400

    add buy now button

--- src/components/HeroSlider/HeroSlide/SlideContent.tsx
+++ src/components/HeroSlider/HeroSlide/SlideContent.tsx
@@ -1,6 +1,7 @@
 import { ContentColorType, ContentDesktopPositionType, ContentMobilePositionType, SliderContentType } from '@/models/HeroSlider'
 import React from 'react'
 import clx from 'classnames'
+import Link from 'next/link'
 
 type Props = {
     content: SliderContentType
@@ -22,19 +23,38 @@ function SlideContent({ content, color }: Props) {
     const containerClassname = clx(
         "absolute w-[86.66vw] md:w-[43vw] xl:w-[620px] flex flex-col z-[10]",
         {
-            "left-1/2 -translate-x-1/2 bottom-[11.11vw] text-center" : mobilePosition == ContentMobilePositionType.BOOTTOMCENTER,
-            "left-1/2 right-auto -translate-x-1/2 top-[11.11vw] text-center" : mobilePosition == ContentMobilePositionType.TOPCENTER,
-            "md:left-20 md:translate-x-0 md:-translate-y-1/2 md:top-1/2 md:text-left" : desktopPosition == ContentDesktopPositionType.LEFTCENTER,
-            "md:right-20 md:left-auto md:translate-x-0 md:-translate-y-1/2 md:top-1/2 md:text-left" : desktopPosition == ContentDesktopPositionType.RIGHTCENTER,
+            "left-1/2 -translate-x-1/2 bottom-[11.11vw] text-center": mobilePosition == ContentMobilePositionType.BOOTTOMCENTER,
+            "left-1/2 right-auto -translate-x-1/2 top-[11.11vw] text-center": mobilePosition == ContentMobilePositionType.TOPCENTER,
+            "md:left-20 md:translate-x-0 md:-translate-y-1/2 md:top-1/2 md:text-left": desktopPosition == ContentDesktopPositionType.LEFTCENTER,
+            "md:right-20 md:left-auto md:translate-x-0 md:-translate-y-1/2 md:top-1/2 md:text-left": desktopPosition == ContentDesktopPositionType.RIGHTCENTER,
             "text-white": color === ContentColorType.WHITE,
             "text-black": color === ContentColorType.BLACK,
         }
     )
 
+    const buynowClassname = clx(
+        "rounded-btn",
+        {
+            "rounded-btn_white": color === ContentColorType.WHITE,
+            "rounded-btn_black": color === ContentColorType.BLACK,
+        }
+    )
     return (
         <div className={containerClassname} >
             <h2 className='text-[8.3333vw]  md:text-[48px] font-bold text-current' >{title}</h2>
             <p className='mt-[4.44vw]  md:mt-6 text-[3.88vw] md:text-[18px] leading-[1.33] text-current' >{description}</p>
+            {/* buttons */}
+            <div className=' flex flex-col-reverse gap-[2.22222vw] mt-[6.66vw] items-center justify-center' >
+                {/* show more */}
+                {/* buy now */}
+                <Link
+                    href={buyNowLink}
+                    className={buynowClassname}
+                >
+                    {"Buy now"}
+                </Link>
+
+            </div>
         </div>
     )
 }
