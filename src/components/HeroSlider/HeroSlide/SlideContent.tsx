7d4537328df91af2990b1b67231daaaa610f850b
Omid <fishvardroid@gmail.com>
2024-08-02 14:04:44 +0400

    add underline button

--- src/components/HeroSlider/HeroSlide/SlideContent.tsx
+++ src/components/HeroSlider/HeroSlide/SlideContent.tsx
@@ -39,13 +39,27 @@ function SlideContent({ content, color }: Props) {
             "rounded-btn_black": color === ContentColorType.BLACK,
         }
     )
+
+    const learnMoreClassname = clx(
+        "underline-btn mt-[2.2222vw] md:mt-0",
+        {
+            "underline-btn_white": color === ContentColorType.WHITE,
+            "underline-btn_black": color === ContentColorType.BLACK,
+        }
+    )
     return (
         <div className={containerClassname} >
             <h2 className='text-[8.3333vw]  md:text-[48px] font-bold text-current' >{title}</h2>
             <p className='mt-[4.44vw]  md:mt-6 text-[3.88vw] md:text-[18px] leading-[1.33] text-current' >{description}</p>
             {/* buttons */}
-            <div className=' flex flex-col-reverse gap-[2.22222vw] mt-[6.66vw] items-center justify-center' >
+            <div className=' flex flex-col-reverse md:flex-row gap-[2.22222vw] md:gap-5 mt-[6.66vw] md:mt-[2.777vw] lg:mt-10 items-center justify-center md:justify-start' >
                 {/* show more */}
+                <Link
+                    href={showMoreLink}
+                    className={learnMoreClassname}
+                >
+                    {"Learn more"}
+                </Link>
                 {/* buy now */}
                 <Link
                     href={buyNowLink}
