5a6a867acde5452dd706b618b984a2e4466d9571
Omid <fishvardroid@gmail.com>
2024-08-10 11:52:10 +0400

    add shoecase types

--- src/models/ShowcaseSlider.ts
+++ src/models/ShowcaseSlider.ts
@@ -1,6 +1,18 @@
-
+import { HeroSrcType } from "./HeroSlider"
 
 export enum BadgeType  {
     NEW = "New",
     SALE = "Sale",
+}
+
+export type IShowcaseItem = {
+    title: string
+    description: string
+    badge: BadgeType | null
+    src: HeroSrcType
+}
+
+export type IShowcaseType = {
+    title: string
+    items: IShowcaseItem[]
 }
