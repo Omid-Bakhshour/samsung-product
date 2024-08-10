6da7fe6887ffd39ad5a86d89ba164de1e1910545
Omid <fishvardroid@gmail.com>
2024-08-10 17:06:26 +0400

    add scale animation

--- src/components/Slider/ShowcaseSlider/slide/FullSlideItem.tsx
+++ src/components/Slider/ShowcaseSlider/slide/FullSlideItem.tsx
@@ -7,7 +7,6 @@ type Props = {
   item: IShowcaseItem
 }
 
-
 function FullSlideItem({
   item,
 }: Props) {
@@ -18,18 +17,18 @@ function FullSlideItem({
   const badge = item.badge
 
   return (
-    <div className='relative inline-block w-full md:w-1/2 h-1/3 md:h-full pr-[4.44vw] md:pr-[1.66vw] pb-[4.44vw] md:pb-[1.66vw] xl:pr-6 xl:pb-6 ' >
+    <div className='relative inline-block w-full md:w-1/2 h-1/3 md:h-full pr-[4.44vw] md:pr-[1.66vw] pb-[4.44vw] md:pb-[1.66vw] xl:pr-6 xl:pb-6 group ' >
       <Link
         href={'/'}
         className='relative inline-block w-[86.66vw] h-[48.88vw] md:w-full md:h-full'
       >
         {/* img */}
-        <div className='absolute left-0 bottom-0 top-0 w-full h-full rounded-[5.55vw] md:rounded-[1.38vw] xl:rounded-[20px] overflow-hidden' >
+        <div className='absolute left-0 bottom-0 top-0 w-full h-full rounded-[5.55vw] md:rounded-[1.38vw] xl:rounded-[20px] !overflow-hidden  ' >
           {
             isDesktopImgValid && (
               <img
                 src={desktopImage}
-                className='w-full hidden md:block  h-full object-cover'
+                className='w-full hidden md:block  h-full object-cover showcase-animation group-hover:scale-105'
                 alt={item.title}
               />
             )
@@ -39,7 +38,7 @@ function FullSlideItem({
             isMobileImgValid && (
               <img
                 src={mobileImage}
-                className='w-full block md:hidden h-full object-cover '
+                className='w-full block md:hidden h-full object-cover showcase-animation group-hover:scale-105'
                 alt={item.title}
               />
             )
@@ -51,10 +50,14 @@ function FullSlideItem({
           <div className='realtive block md:absolute top-auto md:top-[1.66vw] xl:top-6 left-auto md:left-[1.66vw] xl:left-6 mb-[1.11vw] md:mb-0' >
             <Badge type={badge} />
           </div>
-
           {/* title */}
+          <div className='relative block w-full mb-[1.11vw] md:mb-0 md:absolute md:bottom-[5.55vw] xl:bottom-[72px] md:w-[27.77vw] xl:w-[480px]  md:left-1/2 md:-translate-x-1/2' >
+            <span className='text-[3.88vw] md:text-[1.66vw] xl:text-[24px] max-h-[15.83vw] md:max-h-[4.44vw] xl:max-h-16 text-left md:text-center font-bold line-clamp-3' >{item.title}</span>
+          </div>
           {/* desc */}
-
+          <div className='relative max-h-[7.77vw] text-[2.77vw] md:absolute md:bottom-[3.33vw] md:w-[27.77vw] xl:bottom-12 xl:w-[400px] md:text-[0.97vw] xl:text-[14px] md:left-1/2 md:-translate-x-1/2 text-left md:text-center' >
+            <span>{item.description}</span>
+          </div>
         </div>
         {/* buy now button */}
       </Link>
