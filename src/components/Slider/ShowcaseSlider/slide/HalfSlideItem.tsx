9d8eba12cc7cc11f0af2400f2aae8ce67a8c3f67
Omid <fishvardroid@gmail.com>
2024-08-12 19:33:18 +0400

    add half size slide img

--- src/components/Slider/ShowcaseSlider/slide/HalfSlideItem.tsx
+++ src/components/Slider/ShowcaseSlider/slide/HalfSlideItem.tsx
@@ -1,3 +1,4 @@
+import Badge from '@/components/Common/Badge'
 import { IShowcaseItem } from '@/models/ShowcaseSlider'
 import Link from 'next/link'
 import React from 'react'
@@ -14,9 +15,9 @@ function HalfSlideItem({
   const isDesktopImgValid = desktopImage && desktopImage.length > 0
   const isMobileImgValid = mobileImage && mobileImage.length > 0
   const badge = item.badge
-  
+
   return (
-    <div className='w-1/2 md:w-1/4 h-1/3 md:h-1/2 pr-[4.44vw] md:pr-[1.66vw] xl:pr-6 pb-[4.44vw] md:pb-[1.66vw] xl:pb-6 relative inline-block sc-small' >
+    <div className='w-1/2 md:w-1/4 h-1/3 md:h-1/2 pr-[4.44vw] md:pr-[1.66vw] xl:pr-6 pb-[4.44vw] md:pb-[1.66vw] xl:pb-6 relative inline-block sc-small group' >
       <Link
         className='relative w-full h-full inline-block cursor-pointer text-black'
         href={'/'}
@@ -43,11 +44,19 @@ function HalfSlideItem({
               />
             )
           }
-
         </div>
         {/* badge */}
+        <div className='absolute top-[3.88vw] md:top-[1.66vw] xl:top-6 left-[4.44vw] md:left-[1.66vw] xl:left-6' >
+          <Badge type={badge} />
+        </div>
         {/* title */}
+        <div className='w-full  absolute left-1/2 -translate-x-1/2 px-[1.11vw] xl:px-4 bottom-[3.88vw] md:bottom-[5vw] xl:bottom-[72px] mx-auto ' >
+          <span className='text-[3.33vw] md:text-[1.25vw] xl:text-[18px] max-h-[15.83vw] md:max-h-[4.44vw] xl:max-h-16 text-center font-bold line-clamp-3' >{item.title}</span>
+        </div>
         {/* button */}
+        <div className='inline-block md:absolute h-[2.77vw] xl:h-10 bottom-[0.27vw] visible opacity-0 md:group-hover:bottom-[1.66vw] xl:group-hover:bottom-6 md:group-hover:visible md:group-hover:opacity-100 xl:b-1 left-1/2 -translate-x-1/2 sc-anim_btn' >
+          <span className='rounded-btn rounded-btn_black' >buy now</span>
+        </div>
       </Link>
 
     </div>
