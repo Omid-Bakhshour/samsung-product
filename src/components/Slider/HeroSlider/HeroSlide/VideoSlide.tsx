ecd1e50b421795977e6a7b058db2e224d10f4e13
Omid <fishvardroid@gmail.com>
2024-08-14 14:11:23 +0400

    fix safari autoplay

--- src/components/Slider/HeroSlider/HeroSlide/VideoSlide.tsx
+++ src/components/Slider/HeroSlider/HeroSlide/VideoSlide.tsx
@@ -58,6 +58,7 @@ function VideoSlide({
           src={desktopSrc}
           muted
           autoPlay
+          playsInline
         />
       )}
 
@@ -68,6 +69,7 @@ function VideoSlide({
           src={mobileSrc}
           muted
           autoPlay
+          playsInline
         />
       )}
     </div>
