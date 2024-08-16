13f76e6e0a815ce13c90c8badee22c8bae84588c
Omid <fishvardroid@gmail.com>
2024-08-16 14:54:44 +0400

    add key feature type and dummy data

---
+++ src/constants/keyFeatureSlider.tsx
@@ -0,0 +1,83 @@
+import { ContentColorType } from "@/models/HeroSlider"
+
+
+
+export const mobileItems = [
+    {
+        id: 1,
+        tabLabel: "Galaxy Z Fold6",
+        title: "Galaxy Z Fold6",
+        description:"Get an AED 400 e-voucher and exclusive benefits, worth up to AED 834.",
+        learnMore: "/",
+        buyNow: "/",
+        image: {
+            mobile: "",
+            desktop: "",
+        },
+        color: ContentColorType.BLACK,
+
+    },
+    {
+        id: 2,
+        tabLabel: "Galaxy Z Flip6",
+        title: "Galaxy Z Flip6",
+        description:"Get exclusive gifts and benefits worth up to AED 639.",
+        learnMore: "/",
+        buyNow: "/",
+        image: {
+            mobile: "",
+            desktop: "",
+        },
+        color: ContentColorType.BLACK,
+
+    },
+    {
+        id: 3,
+        tabLabel: "Galaxy Unpacked Highlight",
+        title: "Unpacked Highlight",
+        description:"",
+        learnMore: "",
+        buyNow: "",
+        image: {
+            mobile: "",
+            desktop: "",
+        },
+        color: ContentColorType.BLACK,
+
+    },
+    {
+        id: 4,
+        tabLabel: "Galaxy Watch Ultra",
+        title: "Galaxy Watch Ultra",
+        description:"Get free Galaxy Buds2 Pro and 5X Samsung Rewards points worth up to AED 433.",
+        learnMore: "/",
+        buyNow: "/",
+        image: {
+            mobile: "",
+            desktop: "",
+        },
+        color: ContentColorType.WHITE,
+
+    },
+    {
+        id: 5,
+        tabLabel: "Galaxy Buds3 Pro",
+        title: "Galaxy Buds3 Pro",
+        description:"Galaxy AI-powered sound.",
+        learnMore: "",
+        buyNow: "/",
+        image: {
+            mobile: "",
+            desktop: "",
+        },
+        color: ContentColorType.BLACK,
+
+    },
+]
+
+
+
+export const mobileKeyFeature = {
+    title: "Mobile",
+    items: mobileItems
+}
---
+++ src/models/KeyFeatureSlider.ts
@@ -0,0 +1,18 @@
+import { ContentColorType, HeroSrcType } from "./HeroSlider"
+
+export type IKeyFeatureItem = {
+    id: number
+    tabLabel: string
+    title: string
+    description: string
+    learnMore: string
+    buyNow: string
+    image: HeroSrcType
+    color: ContentColorType
+}
+
+
+export type KeyFeature = {
+    title: string
+    items: any[]
+}
