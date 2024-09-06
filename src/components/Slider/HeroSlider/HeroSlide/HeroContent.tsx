47a0af8ea5ddb453cac09f0d3f7b71e8b725c86e
Omid <fishvardroid@gmail.com>
2024-09-06 15:11:00 +0400

    complete key feature slider

--- src/components/Slider/HeroSlider/HeroSlide/HeroContent.tsx
+++ src/components/Slider/HeroSlider/HeroSlide/HeroContent.tsx
@@ -2,7 +2,6 @@ import { ContentColorType, ContentDesktopPositionType, ContentMobilePositionType
 import React from 'react'
 import clx from 'classnames'
 import Link from 'next/link'
-import SlideContent from '../../Common/SlideContent'
 
 type Props = {
     content: SliderContentType
@@ -13,7 +12,11 @@ function HeroContent({ content, color }: Props) {
     if (!content) {
         return <></>
     }
-
+    
+    const title = content.title
+    const description = content.description
+    const showMoreLink = content.showmoreLink
+    const buyNowLink = content.buyNowLink
     const position = content.postion
     const mobilePosition = position?.mobile || ContentMobilePositionType.TOPCENTER
     const desktopPosition = position?.desktop || ContentDesktopPositionType.LEFTCENTER
@@ -29,12 +32,45 @@ function HeroContent({ content, color }: Props) {
         }
     )
 
+    const buynowClassname = clx(
+        "rounded-btn",
+        {
+            "rounded-btn_white": color === ContentColorType.WHITE,
+            "rounded-btn_black": color === ContentColorType.BLACK,
+        }
+    )
+
+    const learnMoreClassname = clx(
+        "underline-btn mt-[2.2222vw] md:mt-0",
+        {
+            "underline-btn_white": color === ContentColorType.WHITE,
+            "underline-btn_black": color === ContentColorType.BLACK,
+        }
+    )
+
     return (
-        <SlideContent
-            color={color}
-            content={content}
-            classname={containerClassname}
-        />
+        <div className={containerClassname} >
+        <h2 className='text-[8.3333vw]  md:text-[48px] font-bold text-current' >{title}</h2>
+        <p className='mt-[4.44vw]  md:mt-6 text-[3.88vw] md:text-[18px] leading-[1.33] text-current' >{description}</p>
+        {/* buttons */}
+        <div className=' flex flex-col-reverse md:flex-row gap-[2.22222vw] md:gap-5 mt-[6.66vw] md:mt-[2.777vw] lg:mt-10 items-center justify-center md:justify-start' >
+            {/* show more */}
+            <Link
+                href={showMoreLink}
+                className={learnMoreClassname}
+            >
+                {"Learn more"}
+            </Link>
+            {/* buy now */}
+            <Link
+                href={buyNowLink}
+                className={buynowClassname}
+            >
+                {"Buy now"}
+            </Link>
+
+        </div>
+    </div>
     )
 }
 
--- src/components/Slider/KeyFeatureSlider/Content/KeyFeatureSlide/KeyFeatureContent.tsx
+++ src/components/Slider/KeyFeatureSlider/Content/KeyFeatureSlide/KeyFeatureContent.tsx
@@ -1,8 +1,8 @@
-import SlideContent from '@/components/Slider/Common/SlideContent'
 import { ContentColorType } from '@/models/HeroSlider'
 import { IKeyFeatureItem } from '@/models/KeyFeatureSlider'
 import React from 'react'
-
+import clx from 'classnames'
+import Link from 'next/link'
 
 type Props = {
     sliderItem: IKeyFeatureItem
@@ -13,17 +13,69 @@ function KeyFeatureContent({
     sliderItem,
     activeColor,
 }: Props) {
-    return (
-        <SlideContent
-            color={activeColor}
-            content={{
-                title: sliderItem.title,
-                description: sliderItem.description,
-                buyNowLink: sliderItem.buyNow,
-                showmoreLink: sliderItem.learnMore
-            }}
-        />
 
+    if (!sliderItem) {
+        return <></>
+    }
+
+    const title = sliderItem.title
+    const description = sliderItem.description
+    const showMoreLink = sliderItem.learnMore
+    const buyNowLink = sliderItem.buyNow
+
+    const containerClassname = clx(
+        "absolute left-0 bottom-[15vw] md:bottom-[3.33vw] xl:bottom-11 px-[6.66vw] md:px-[1.66vw] xl:px-6 w-full flex flex-col items-center justify-center z-[2]",
+        {
+
+            "text-white": activeColor === ContentColorType.WHITE,
+            "text-black": activeColor === ContentColorType.BLACK,
+        }
+    )
+
+    const buynowClassname = clx(
+        "rounded-btn",
+        {
+            "rounded-btn_white": activeColor === ContentColorType.WHITE,
+            "rounded-btn_black": activeColor === ContentColorType.BLACK,
+        }
+    )
+
+    const learnMoreClassname = clx(
+        "underline-btn mt-[2.2222vw] md:mt-0",
+        {
+            "underline-btn_white": activeColor === ContentColorType.WHITE,
+            "underline-btn_black": activeColor === ContentColorType.BLACK,
+        }
+    )
+
+    return (
+        <div className={containerClassname} >
+            <h2 className='mb-[2.22vw] md:mb-[0.55vw] xl:mb-2  text-[8.3333vw] md:text-[3.33vw] xl:text-[48px] font-bold text-current text-center' >{title}</h2>
+            {
+                description && description.length > 0 && (
+                    <p className='mb-[2.22vw] md:mb-[0.55vw] xl:mb-2 text-[3.88vw] md:text-[1.25vw] xl:text-[18px] leading-[1.33] text-current text-center' >{description}</p>
+
+                )
+            }
+            {/* buttons */}
+            <div className=' flex flex-col-reverse md:flex-row gap-[2.22222vw] md:gap-5 items-center justify-center ' >
+                {/* show more */}
+                <Link
+                    href={showMoreLink}
+                    className={learnMoreClassname}
+                >
+                    {"Learn more"}
+                </Link>
+                {/* buy now */}
+                <Link
+                    href={buyNowLink}
+                    className={buynowClassname}
+                >
+                    {"Buy now"}
+                </Link>
+
+            </div>
+        </div>
     )
 }
 
