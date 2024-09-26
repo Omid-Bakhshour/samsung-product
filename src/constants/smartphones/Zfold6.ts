d349807da77a56a1b22b15dac2c1cf2f764da7c5
Omid <fishvardroid@gmail.com>
2024-09-26 14:32:39 +0400

    add navigation menu items

--- src/constants/smartphones/Zfold6.ts
+++ src/constants/smartphones/Zfold6.ts
@@ -21,32 +21,56 @@ export const zfold6UtilitiyLinks = [
 
 export const headlineList = [
     {
-        id: 1,
+        id: 4,
         label: "Galaxy Z Fold6",
         link: "/"
     },
     {
-        id: 2,
+        id: 5,
         label: "Galaxy Z Flip6",
         link: "/"
     },
     {
-        id: 3,
+        id: 6,
         label: "Galaxy Watch Ultra",
         link: "/"
     }, {
-        id: 4,
+        id: 7,
         label: "Galaxy Watch7",
         link: "/"
     },
     {
-        id: 5,
+        id: 8,
         label: "Galaxy Buds3 Pro",
         link: "/"
     },
     {
-        id: 6,
+        id: 9,
         label: "See all Foldables",
         link: "/"
     },
+]
+
+export const menuLink = [
+    {
+        id: 4,
+        label: "Features",
+        link: "/features/"
+    },
+    {
+        id: 3,
+        label: "Compare & Upgrade",
+        link: "/reviews/"
+    },
+    {
+        id: 3,
+        label: "Switch to galaxy",
+        link: "/reviews/"
+    },
+    {
+        id: 3,
+        label: "Accessories",
+        link: "/reviews/"
+    },
+
 ]
